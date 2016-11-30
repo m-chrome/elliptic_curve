489469c42c2cde49fe8051bd0989aab744bf6927
Michail Tsyganov <tsyganov.michail@yandex.ru>
2016-11-30 16:24:16 +0300

    Макросы для удобства

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 #include <iostream>
 #include <gcrypt.h>
 
+#define MODE_GOST 0
+#define MODE_RANDOM 1
+
 using namespace std;
 
 int main()
 {
     EllipticCurve curve;
-    curve.build_point(0);
+    curve.build_point(MODE_RANDOM);
     Point DP = curve.doubling_point(curve.P);
     if (curve.check_projective_point_belongs(DP) == 0)
         cout << "Операция удвоения работает.\n";
